25082fbf3ac71140c0cef8b27c39f42c977ad377
Flick <anhnv@kztek.net>
2022-03-29 23:00:58 +0700

    First Commit

---
+++ NetworkTools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace SocketHelper
+{
+    public class NetWorkTools
+    {
+        public static bool IsPingSuccess(string ipAddress, int timeOut)
+        {
+            Ping pingSender = new Ping();
+            PingReply reply = pingSender.Send(ipAddress, timeOut);
+            if (reply != null && reply.Status == IPStatus.Success)
+                return true;
+            else
+                return false;
+        }
+    }
+}
